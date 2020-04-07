12b26ba3d47d22a84d20520cd7ea9e0ed567fada
sist <sist@DESKTOP-QK2CKFJ>
2020-04-07 10:50:56 +0900

    ㅇ예에

--- eHRGir0101/src/com/sit/ehr/eHRGit0101.java
+++ eHRGir0101/src/com/sit/ehr/eHRGit0101.java
@@ -5,6 +5,7 @@ public class eHRGit0101 {
 	public static void main(String[] args) {
 		System.out.println("Hellow World");
 		System.out.println("Hellow JuC~~~");
+		System.out.println("nanigananikazenzenwakaran");
 		 
 		}
 }
